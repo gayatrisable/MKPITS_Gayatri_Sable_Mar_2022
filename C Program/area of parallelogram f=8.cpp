2fdf19645868f4f6a8432e64f8eda550021f9098
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-04-16 06:45:13 -0700

    Add files via upload

---
+++ C Program/area of parallelogram f=8.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the value of side and height=");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	printf("Area of Parallelogram=%d",c);
+}
