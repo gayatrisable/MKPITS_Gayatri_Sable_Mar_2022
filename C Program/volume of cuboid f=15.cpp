da5ac90296846656deffd847b1b1c45685536f2d
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-04-16 06:52:08 -0700

    Add files via upload

---
+++ C Program/volume of cuboid f=15.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,b,c;
+ printf("Enter the value of l,b,h=");
+ scanf("%d%d%d",&a,&b,&c);
+ int v=a*b*c;
+ printf("Volume of cuboid=%d",v);
+}
