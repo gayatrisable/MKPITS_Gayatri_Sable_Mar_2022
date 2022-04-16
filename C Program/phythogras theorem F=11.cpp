76898da7904ec5a39a119dfed74f236ff0f34eb2
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-04-16 06:49:42 -0700

    Add files via upload

---
+++ C Program/phythogras theorem F=11.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the value of three side=");
+	scanf("%d%d%d",&c,&a,&b);
+	int d=c*c;
+	int e=((a*a)+(b*b));
+	printf("Phythogra's Theorem is %d=%d",d,e);
+	
+}
