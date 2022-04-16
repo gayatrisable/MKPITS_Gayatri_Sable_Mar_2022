7ca3b27cb953760f895e793cf743827b8bb42b95
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-04-16 06:50:09 -0700

    Add files via upload

---
+++ C Program/volume of cone f=12.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include <conio.h>
+ int main()
+ {
+ 	int a;
+	 float pi=3.14;
+ 	printf("Enter the value radi=");
+ 	scanf("%d",&a);
+ 	float c=(pi*a*a*a)/3;
+ 	printf("Volume of cone=%f",c);
+ }
