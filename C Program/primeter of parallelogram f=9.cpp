e35b7360d02a1bf222d55429e870d5ce5e6b0553
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-04-16 06:45:44 -0700

    Add files via upload

---
+++ C Program/primeter of parallelogram f=9.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter the vale of width and height=");
+	scanf("%d%d",&a,&b);
+	c=2*(a+b);
+	printf("Perimeter of parallogram=%d",c);
+}
