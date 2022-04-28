178e1add66f829ea42f75e31fe56587f25dc2222
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-04-28 13:25:29 +0530

    Add files via upload

---
+++ C Program/pattern H..W=10.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int r,sp,str;
+	for(r=1;r<5;r++)
+	{
+		for(r=1;r<=10;r++)
+		{
+			printf("");
+		}
+		for(str=1;str<=r;str++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
