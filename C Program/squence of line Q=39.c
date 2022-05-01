b1187c0e02c9036d3fcb3ea0781901c32438f5c6
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-05-01 05:28:03 -0700

    Add files via upload

---
+++ C Program/squence of line Q=39.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	printf("Enter number of lines:");
+	scanf("%d",&a);
+	printf("Number of numberin a line:");
+	scanf("%d",&b);
+	for(c=1,f=1;c<=a;c++)
+	{
+		{
+			for(e=1;e<=b;e++)
+			{
+		    printf("%d  ",f);
+		f++;
+	}
+	printf("\n");
+}
+}
+}
