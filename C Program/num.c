91d1ce8fe18f59738e4a4264d59b55253c5e82da
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-05-05 04:54:09 -0700

    Add files via upload

---
+++ C Program/num.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+int x1,x2;//global decleration
+int fun();
+int main()
+{
+	int s;
+	s=fun();
+	if(s==0)
+	{
+	 printf("%d is smaller",x1);	
+	}
+	else
+	{
+		printf("%d is smaller",x2);
+	}
+}
+//returntype and no arguement
+int fun()
+{
+	printf("Enter two number\n");
+	scanf("%d%d",&x1,x2);
+	if(x1<x2)
+	{
+		return 0;
+	}
+	else
+	{
+		return 0;
+	}
+	}	
