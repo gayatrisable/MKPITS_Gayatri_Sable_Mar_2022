2e8d80fd29e71daf9ad575e4fa660ea4673948b5
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-05-09 10:00:15 -0700

    Add files via upload

---
+++ C Program/multiplication of 3 digit Q=73.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("Enter the 1st value: ");
+	scanf("%d",&a);
+	printf("Enter the 2nd value: ");
+	scanf("%d",&b);
+    printf("Enter the 3rd value: ");
+	scanf("%d",&c);
+	d=a*b*c;
+	printf("multiplication of 3 digit:%d ",d);
+}
