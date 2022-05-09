52d1d551fb618134c3d44a718eb1661774723a66
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-05-09 09:56:36 -0700

    Add files via upload

---
+++ C Program/power of 2 table Q=60.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,pow=1;
+	float neg;
+	for(i=1;i<=10;i++)
+	{
+		if(i==0)
+		{
+			pow=1;
+		}
+		else
+		{
+			pow=pow*2;
+			neg=1.0/pow;
+		}
+		printf("%d  %d  %f\n",i,pow,neg);
+	}
+}
