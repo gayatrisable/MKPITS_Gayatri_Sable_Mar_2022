a5667cfbad93e3a2c6b81262532fc439fe1cd7a0
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-05-14 08:01:43 -0700

    Add files via upload

---
+++ C Program/range 10,100,200 Q=78.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	printf("Enter the value:");
+	scanf("%d",&n);
+	
+	if(n<=200 && n>10)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
+}
