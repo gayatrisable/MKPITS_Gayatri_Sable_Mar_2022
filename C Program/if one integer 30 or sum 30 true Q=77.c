f138e4629d8cafe7a61baefa9d33e9968df82d2b
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-05-14 08:01:12 -0700

    Add files via upload

---
+++ C Program/if one integer 30 or sum 30 true Q=77.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter two value:");
+	scanf("%d%d",&a,&b);
+	if(a==30||b==30||a+b==30)
+	{
+	 printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
+}
