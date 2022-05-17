eef9f3720911d61d376a0867bcc506146b8dc0c2
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-05-16 23:37:13 -0700

    Add files via upload

---
+++ C Program/given num is witb in 2 of a multiple of 10 Q=95.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	printf("Enter Any Number: ");
+	scanf("%d",&n);
+	
+	if(n%10<=2 || n%10>=8)
+	{
+		printf("Within the to of multiple of 10");
+	}
+	else
+	{
+		printf("Not Within the to of multiple of 10");
+	}
+}
