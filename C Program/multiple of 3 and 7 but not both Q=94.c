371c16f6ca06b391cda2000eee5bedd9a151c147
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-05-16 23:35:37 -0700

    Add files via upload

---
+++ C Program/multiple of 3 and 7 but not both Q=94.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	printf("Enter Any Number: ");
+	scanf("%d",&n);
+	
+	if(n%3==0 && n%7==0)
+	{
+		printf("Not Multiple Of 3 or 7");
+	}
+	else
+	if(n%3==0)
+	{
+		printf("Multiple Of 3");
+	}
+	else
+	if(n%7==0)
+	{
+		printf("Multiple Of 7");
+	}
+	else
+	{
+      printf("Not Multiple Of 3 or 7");
+    }
+}
