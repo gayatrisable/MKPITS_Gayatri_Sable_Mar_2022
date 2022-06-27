7e20473ea2b9c94e55669ccae1671c3f0dde5329
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-06-27 21:00:58 +0530

    Add files via upload

---
+++ C Program/Q126.c
@@ -0,0 +1,21 @@
+//write a c program to check if an array of integers contains a 3 next to a 5 next to a 5 or both.
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a[5],i,count=0;
+	printf("enter 5 array elements :");
+	for(i=0;i<5;i++)
+	{
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		if(a[i]==3 || a[i+1]==3|| a[i]==5 || a[i+1]==5)
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+}
