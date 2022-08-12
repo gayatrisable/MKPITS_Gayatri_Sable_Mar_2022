3ba418376548c1f749d87b192a8ed523ff3c78cc
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-12 18:04:36 +0530

    Add files via upload

---
+++ C Program/Q=131.c
@@ -0,0 +1,30 @@
+//Write a c program to check a given array of interger and return true if every 5 that appears in the given array is next to anoyher 5.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n,f=0,temp,a[10];
+	printf("enter array size: ");
+	scanf("%d",&n);
+	printf("enter array element\n");
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d]= ",i);
+		scanf("%d",&a[i]);
+	}
+	i=0;
+	temp=a[0];
+	while(a[i]==temp&&a[i]==a[(n-1)-i])
+	{
+		f=f+1;
+		i++;
+	}
+	if(f>0)
+	{
+		printf("true");
+	}
+	else
+	{
+		printf("false");
+	}
+}
