283a0b19654d555888406cff9b78fff129fb9bd4
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 12:04:05 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a15.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a15.prime_number_by_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept a no. and print whether it is primeno or not.
+            int i, num, cnt = 0;
+            Console.WriteLine("enter number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    cnt++;
+                }
+            }
+            switch (cnt)
+            {
+                case 2:
+                    Console.WriteLine("Prime number");
+                    break;
+                default:
+                    Console.WriteLine("Not a prime number");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
