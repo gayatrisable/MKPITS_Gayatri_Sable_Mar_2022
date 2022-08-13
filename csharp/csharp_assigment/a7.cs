4d81a86fdf8f8b77ccd406d556dd9172c2453686
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 11:59:27 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a7.bonous_if_else_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //8 wap to empno, empname , designation calculate and display bonus ie 10000 for manager 5000 for cleark 2000 for peon
+            string name, id,des;
+            Console.WriteLine("Enter employee id:");
+            id = Console.ReadLine();
+            Console.WriteLine("Enter employee name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter designation:");
+            des = Console.ReadLine();
+            if(des=="manager")
+            {
+                Console.WriteLine("Manager Bonous: "+10000);
+            }
+            else if (des == "cleark")
+            {
+                Console.WriteLine("Cleark Bonous: " + 5000);
+            }
+            else if (des =="peon")
+            {
+                Console.WriteLine("Peon Bonous: " + 2000);
+            }
+            else
+            {
+                Console.WriteLine("please enter valid designation!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
