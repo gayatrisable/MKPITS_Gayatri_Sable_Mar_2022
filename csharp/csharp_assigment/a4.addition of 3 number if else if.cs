83588ac7e794d3fc30f243b5151f48b88d3f7607
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 11:58:01 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a4.addition of 3 number if else if.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a4_addition_of_3_num_if_else_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5- wap to accept 3 no. and display greater no.
+            int num1, num2, num3;
+            Console.WriteLine("Enter the 1st number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 1st number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 1st number:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num1 > num3) {
+                Console.WriteLine(num1+" is greater number");
+            }
+            else if(num2>num1 && num2 > num3)
+            {
+                Console.WriteLine(num2 + " is greater number");
+            }
+            else
+            {
+                Console.WriteLine(num3 + " is greater number!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
