b01d63d77d1cf6013d9ae43021b70be32e4e5d94
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 12:00:52 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a10. even or odd if else.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a10.even_or_odd_if_else_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept a no. and print whether it is even no or oddno.
+            int num;
+            Console.WriteLine("Enter any number");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("even number");
+
+            }
+            else
+            {
+                Console.WriteLine("odd number");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
