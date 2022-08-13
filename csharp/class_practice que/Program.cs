b0f083862dd77b51dc8af9fd9602d052f2f3c96b
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 10:54:54 +0530

    hello word

---
+++ csharp/class_practice que/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO GAYATRI!HOW ARE U");
+            Console.ReadLine();
+        }
+    }
+}
