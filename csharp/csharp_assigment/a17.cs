e314eff1992a237b9b925e68c7b23c4ede8a33fa
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 12:05:09 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a17.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a1_print_table_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //program to print table using  while
+            int i=1, n, res;
+            Console.WriteLine("Enter Number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (i <= 10) 
+            {
+                res = i * n;
+                Console.WriteLine(n + " * " + i + " = " + res);
+                i++;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
