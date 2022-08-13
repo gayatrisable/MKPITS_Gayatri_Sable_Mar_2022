1fb87f4d3bee1cd4e10a6b272bbedb9dada22391
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 12:02:35 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a12.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a._12_empolyee_by_switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //8 wap to empno, empname , dsesignation calculate and display bonus ie 10000 for manager 5000 for clerk 2000 for peon by switch
+            
+            string name, des;
+            Console.WriteLine("enter Employee name");
+            name = Console.ReadLine();
+
+            Console.WriteLine("enter Designation");
+            des = Console.ReadLine();
+            switch (des)
+            {
+                case "manager":
+                    Console.WriteLine("Bonus: 10000");
+                    break;
+                case "clerk":
+                    Console.WriteLine("Bonus: 5000");
+                    break;
+                case "peon":
+                    Console.WriteLine("Bonus: 2000");
+                    break;
+                default:
+                    Console.WriteLine("Invalid designation");
+                    break;
+            }
+            Console.ReadLine();
+        }   
+    }
+}
