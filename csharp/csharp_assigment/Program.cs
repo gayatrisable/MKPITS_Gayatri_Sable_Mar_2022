8835055cc8edf45ea316a232e7cb5aafc44d544c
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 10:59:51 +0530

    addition of 3 number

--- csharp/csharp_assigment/Program.cs
+++ csharp/csharp_assigment/Program.cs
@@ -4,20 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace area_of_reactangle
+namespace addition_of_3_number
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int num1, num2;
-            Console.WriteLine("Enter the Length:");
+            int num1, num2, num3;
+            Console.WriteLine("Enter the 1st Number:");
             num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the breadth:");
+            Console.WriteLine("Enter the 2nd Number:");
             num2 = Convert.ToInt32(Console.ReadLine());
-            int a = num1 * num2;
-            Console.WriteLine("Area of Rectangle:"+ a);
+            Console.WriteLine("Enter the 3rd Number:");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            int r = num1 + num2 + num3;
+            Console.WriteLine("Addition of three Number"+ r);
             Console.ReadLine();
+
         }
     }
 }
