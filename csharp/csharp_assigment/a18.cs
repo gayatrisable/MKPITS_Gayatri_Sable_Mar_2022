4d45582ec984948cc9cc7fc385110aa5bbf748de
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 12:05:29 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a18.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a2.print_table_by_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // print table by do while
+            int i = 1, n, res;
+            Console.WriteLine("Enter the number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                res = n * i;
+                Console.WriteLine(n+ " * " +i+ " = " +res);
+                i++;
+            } while (i <= 10);
+            Console.ReadLine();
+        }
+    }
+}
