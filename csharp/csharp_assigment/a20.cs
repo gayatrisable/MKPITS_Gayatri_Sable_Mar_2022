73c3e67931150297a20634532213a600d2cb9a54
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 12:06:21 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a20.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a4.factorial_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // print factorial do while
+            int i, n, fact = 1;
+            Console.WriteLine("Enter number");
+            n = Convert.ToInt32(Console.ReadLine());
+            i = n;
+            do
+            {
+                fact = fact * i;
+                i--;
+            } while (i > 0);
+            Console.WriteLine(fact);
+            Console.ReadLine();
+        }
+    }
+}
