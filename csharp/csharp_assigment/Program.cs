ae4b30ce1d3362ea61dea8a1a971788c7bd649a9
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 10:58:50 +0530

    area of rectangle

---
+++ csharp/csharp_assigment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area_of_reactangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Enter the Length:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the breadth:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int a = num1 * num2;
+            Console.WriteLine("Area of Rectangle:"+ a);
+            Console.ReadLine();
+        }
+    }
+}
