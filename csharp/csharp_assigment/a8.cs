68017fba354eb4c53d80cc5e2533af6e51d36a35
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 11:59:59 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a8.product_50_dissount_if_else_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept productname, productrate, quantity calculate totalamount, discount ie 50 % if totalamount > 2000 else discount = 10 % of total amount do with if
+            string pro;
+            int q, r;
+            Console.WriteLine("Enter product Name:");
+            pro = Console.ReadLine();
+            Console.WriteLine("Quantity:");
+            q = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Product Rate:");
+            r = Convert.ToInt32(Console.ReadLine());
+            int total = r * q;
+            Console.WriteLine("Product Name: " + pro);
+            Console.WriteLine("total= " + total);
+            
+            if(total>2000)
+            {
+                float a = total - (total * 50 / 100);
+                
+                Console.WriteLine("toatl Amount: " +a);
+            }
+            else 
+            {
+                float b = total -(total* 10 / 100);
+
+                Console.WriteLine("toatl Amount: " + b);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
