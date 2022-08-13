5e07258c41e3340214e789f21367c0add7ba6cd0
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 11:19:39 +0530

    assigment

---
+++ csharp/csharp_assigment/vowel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a9.vowel_if_else_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept character from the user and display whether it is vowel or not.
+            char ch;
+            Console.WriteLine("Enter the charachter");
+            ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + " is Vowel.");
+            }
+            else
+            {
+                Console.WriteLine("it is not vowel!");
+            }
+
+            Console.ReadLine();
+          }
+    }
+
+     }
+
