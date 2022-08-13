9566f02daf86830fec1127f6f1cc4ad49804b1d0
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 12:07:10 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a22.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a6.prime_number_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //print prime number 1 to 20
+            int a, b;
+            for (a = 1; a <= 20; a++)
+            {
+                int cnt = 0;
+                for (b = 1; b <= a; b++)
+                {
+                    if (a % b == 0)
+                    {
+                        cnt++;
+
+                    }
+                }
+                if (cnt == 2)
+                {
+                    Console.Write(a + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
