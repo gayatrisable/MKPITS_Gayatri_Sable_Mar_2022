e549c65c7ac80567e85970962faba7a1511ca414
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-13 12:08:57 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/a26.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a4.object_fields_make_model_and_method
+{//4- create a class car having fields make,model and method getcardata and displaycardata
+    public class Car
+    {
+        public string company_name;
+        public string model;
+        public void getcardata(string company_name, string model)
+        {
+            this.company_name = company_name;
+            this.model = model;
+        }
+        public void displaycardata()
+        {
+            Console.WriteLine("Company name: " + company_name);
+            Console.WriteLine("model: " + model);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Console.Write("enter company name: ");
+            string company_name = Console.ReadLine();
+            Console.Write("enter model: ");
+            string model = Console.ReadLine();
+            car.getcardata(company_name, model);
+            Console.WriteLine("");
+            car.displaycardata();
+            Console.ReadLine();
+        }
+    }
+}
