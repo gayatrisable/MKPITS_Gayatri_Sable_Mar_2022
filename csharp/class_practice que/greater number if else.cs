046840a80229fdaf4176e9035a4c9a702dcaea37
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:32:42 +0530

    Add files via upload

---
+++ csharp/class_practice que/greater number if else.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace greater_number_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // to accept 2 no. and print greater no.by if else
+            int a, b;
+            Console.WriteLine("Enter the first number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a+" is greater number");
+
+            }
+            else
+            {
+                Console.WriteLine(b + " is greater number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
