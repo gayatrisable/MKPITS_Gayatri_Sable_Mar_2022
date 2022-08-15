04163238b95dc4224bf52a54cba05b0018b5e360
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:20:24 +0530

    Add files via upload

---
+++ csharp/class_practice que/array index of method.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indexof_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 3, 5, 1, 2, 4 };
+
+            int l = Array.IndexOf(num, 2);
+            Console.WriteLine("index " + l);
+            Console.ReadLine();
+        }
+    }
+}
