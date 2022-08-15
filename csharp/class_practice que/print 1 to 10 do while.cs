87e0b21526935a19652e1f7bda8ab9fdc3bd8d57
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:37:52 +0530

    Add files via upload

---
+++ csharp/class_practice que/print 1 to 10 do while.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_1_to_10_do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 10; n++)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadLine();
+        }
+    }
+}
