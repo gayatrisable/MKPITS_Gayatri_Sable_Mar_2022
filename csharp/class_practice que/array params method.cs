f99385f0c1aabb07faa3a4aa1ccf7b07c829f942
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:22:02 +0530

    Add files via upload

---
+++ csharp/class_practice que/array params method.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @params
+{
+    internal class Program
+    {                                                //example of params is used to multiple array dislay
+        static void display(params int[] num)//syntax of params
+        {
+            Console.WriteLine(num[0]);
+            Console.WriteLine(num[1]);
+            Console.WriteLine(num[2]);
+
+        }
+        static void Main(string[] args)
+        {
+            display(2, 3, 4);
+            Console.ReadLine();
+        }
+        
+    }
+}
