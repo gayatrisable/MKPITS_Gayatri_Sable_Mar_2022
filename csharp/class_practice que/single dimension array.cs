e608ac57815b7c54138f47fc54e9a7e98d8f699a
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:39:52 +0530

    Add files via upload

---
+++ csharp/class_practice que/single dimension array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace single_dimension_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a num array of 5 elements
+            int[] num = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter number");
+                num[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //display the values of array
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("num [ {0} ] = {1}", i, num[i]);
+                Console.ReadLine();
+            }
+        }
+    }
+}
