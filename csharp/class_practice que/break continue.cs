9bbfdb03c20e0c37c2ddf46f5b2e4d1875770e32
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:24:26 +0530

    Add files via upload

---
+++ csharp/class_practice que/break continue.cs
@@ -0,0 +1,30 @@
+﻿using System//;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace break_continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The break statement jumps out of an iteration
+            //The continue statement jumps to the next iteration
+            int i = 1;
+            while (true)
+            {
+                Console.WriteLine(i);
+
+                i++;
+                if (i <= 20)
+                    continue;
+                else
+                    break;
+            }
+            Console.WriteLine("bye");
+            Console.ReadLine();
+        }
+    }
+}
