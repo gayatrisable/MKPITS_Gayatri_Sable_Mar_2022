318450753c2b3b60af6abb1d5d2e4302d30043f1
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:41:14 +0530

    Add files via upload

---
+++ csharp/class_practice que/with parameter no reaturn type.cs
@@ -0,0 +1,27 @@
+﻿//method with parameter and no return type
+//wap to create a method to square a no.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace with_parameter_and_no_return_type
+{
+    internal class Program
+    {
+        void square()
+        {
+            int num = 5;
+            int sq = num * num;
+            Console.WriteLine("square = " + sq);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.square(); 
+            Console.WriteLine("THANK U!");
+            Console.ReadLine();
+        }
+    }
+}
