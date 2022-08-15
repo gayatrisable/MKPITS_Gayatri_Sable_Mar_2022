a09a5dcae500f51a78422bce476d46ec6f29fc7c
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:32:10 +0530

    Add files via upload

---
+++ csharp/class_practice que/goto even odd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace goto_even_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //goto even odd
+            int num ;
+            Console.WriteLine("enter number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0) goto even;
+            Console.WriteLine("this is odd number");
+            goto end;
+
+        even:
+            Console.WriteLine("number is even");
+            goto end;
+        end:
+            Console.WriteLine("Thank you!");
+            Console.ReadLine();
+        }
+    }
+}
