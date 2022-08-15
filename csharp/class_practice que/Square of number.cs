20dd53556e4818186309005f8b5096b8f5462c3d
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:40:24 +0530

    Add files via upload

---
+++ csharp/class_practice que/Square of number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square_of_num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+            int res = num * num;
+            Console.WriteLine("Square:" + res);
+            Console.ReadLine();
+        }
+    }
+}
