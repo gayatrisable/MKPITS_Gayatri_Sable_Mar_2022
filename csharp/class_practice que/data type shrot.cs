fa373f295de5e63e4f479c2ca5c914afa9e242ad
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:29:39 +0530

    Add files via upload

---
+++ csharp/class_practice que/data type shrot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_type_short
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            short s;
+            Console.WriteLine("Enter short data:");
+            s = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("short data is: " + s);
+            Console.ReadLine();
+        }
+    }
+}
