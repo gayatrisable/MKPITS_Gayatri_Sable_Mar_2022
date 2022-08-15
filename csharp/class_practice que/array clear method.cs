f10ce0ea23aaa2cdb67f0d6b828afec024ddc1c5
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:18:34 +0530

    Add files via upload

---
+++ csharp/class_practice que/array clear method.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clear_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 3, 5, 1, 2, 4 };
+
+            Array.Clear(num, 0, 2);
+
+            //display the values of array
+            for (int i = 0; i < num.Length; i++)
+            {
+
+                Console.Write(num[i] + "\t");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
