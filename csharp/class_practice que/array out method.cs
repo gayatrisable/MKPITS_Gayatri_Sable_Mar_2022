c9b1df9abe4da6751269dd9eea72b90466473e6c
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:21:35 +0530

    Add files via upload

---
+++ csharp/class_practice que/array out method.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @out
+{
+    internal class Program
+    {
+        static void display(out int num)//out
+        {                                                         //example of out
+            num = 5;
+
+            Console.WriteLine("num inside method " + num); //5
+
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            display(out num);//out
+
+            Console.WriteLine("num inside main" + num);//5
+            Console.ReadLine();
+        }
+    }
+}
