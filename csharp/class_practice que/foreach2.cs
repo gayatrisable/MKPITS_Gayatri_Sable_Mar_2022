10a1268696c21a53edb706e9b16323c60ae32b92
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:31:42 +0530

    Add files via upload

---
+++ csharp/class_practice que/foreach2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreach_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //example of foreach loop
+            //create an array of 5 element
+            string[] num = new string[3];
+
+            num[0] = "amit";
+            num[1] = "raj";
+            num[2] = "dilip";
+            //using foreach loop to iterate over the elements of an array
+            foreach (string i in num)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
