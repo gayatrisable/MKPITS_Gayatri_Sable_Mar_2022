9440b1cd55b03b06a3fc6dfc47bdac1ecb196cb7
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:43:35 +0530

    without parameter return value

---
+++ csharp/class_practice que/without parameter return value.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace without_parameters_and_returning_value
+{
+    internal class Program
+    {
+        static int square()
+        {
+            //method without parameters and returning value
+            //wap to create a method to square a no.
+            int num = 5;
+            int sq = num * num;
+            return sq;
+        }
+        static void Main(string[] args)
+        {
+            int res = square(); 
+            Console.WriteLine("square " + res);
+            Console.WriteLine("THANK U!");
+            Console.ReadLine();
+        }
+    }
+}
