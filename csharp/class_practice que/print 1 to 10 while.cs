e9c3bcd2bf9412526a1c9dfa4e85e9d9b82e3ab3
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:38:25 +0530

    Add files via upload

---
+++ csharp/class_practice que/print 1 to 10 while.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print_1_to_10_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //program to print no from 1 to 10 using while loop
+            int n = 1;
+            while (n <= 10)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
