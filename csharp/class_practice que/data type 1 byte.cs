0e5dc938bbdd35dc8ed88f9f060f9d22a1b31bc0
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:27:42 +0530

    Add files via upload

---
+++ csharp/class_practice que/data type 1 byte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_type_1_byte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b1;
+            Console.WriteLine("enter bite ");
+            b1 = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("byte entered = " + b1);
+
+            Console.ReadLine();
+        }
+    }
+}
