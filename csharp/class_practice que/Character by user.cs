0519b173fbb59fcca4e642d7dbe7f406511b17f3
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:24:56 +0530

    Add files via upload

---
+++ csharp/class_practice que/Character by user.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace character_by_user
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("enter your name ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("name entered = " + name);
+
+            Console.ReadLine();
+        }
+    }
+}
