aaa33fa37f371a43e9ae6865bb204d0f30301d70
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:36:52 +0530

    Add files via upload

---
+++ csharp/class_practice que/operator if else.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operator_if_else_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //wap to accept 2 no and operator like + - * and display the result
+            int s1, s2;
+            char op;
+            int res = 0;
+            Console.WriteLine("enter num1 ");
+            s1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter num2 ");
+            s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter operator like + - * ");
+            op = Convert.ToChar(Console.ReadLine());
+            if (op == '+')
+                res = s1 + s2;
+            else if (op == '-')
+                res = s1 - s2;
+            else if (op == '*')
+                res = s1 * s2;
+            else
+                Console.WriteLine("invalid operator");
+
+            Console.WriteLine("result = " + res);
+            Console.ReadLine();
+        }
+    }
+}
