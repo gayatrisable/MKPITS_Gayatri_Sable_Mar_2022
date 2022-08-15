830c4b7b4ce6e4d375d82ae2f78f54726807ddab
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:34:23 +0530

    Add files via upload

---
+++ csharp/class_practice que/method  reaturing an array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_returning_an_array
+{
+    internal class Program
+    {
+        static int[] returnarray()
+        {
+            int[] num = { 1, 2, 3,4,5 };
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = returnarray();
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
