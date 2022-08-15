edf935852bb8a680c3f7688cf73a165bca1ff7be
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:22:45 +0530

    Add files via upload

---
+++ csharp/class_practice que/array pass by reference value.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pass_by_reference
+{
+    internal class Program
+    {                                                                    //pass by reference example
+        static void display(ref int num)//reference
+        {
+            num++;
+            Console.WriteLine("num inside method " + num); //6
+
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            display(ref num);//reference
+
+            Console.WriteLine("num inside main" + num); //6
+            Console.ReadLine();
+        }
+    }
+}
