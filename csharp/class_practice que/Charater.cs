cf8fd2e41770cef9ccc1877dff118a1593890fd9
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:25:18 +0530

    Add files via upload

---
+++ csharp/class_practice que/Charater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("enter character ");
+            ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("character entered = " + ch);
+
+            Console.ReadLine();
+        }
+    }
+}
