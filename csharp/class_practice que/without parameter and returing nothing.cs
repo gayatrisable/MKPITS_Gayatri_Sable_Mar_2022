2bd208ef5ec7ae20054fd15540bdb4aaa9ff1ab6
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:42:47 +0530

    without parameter and returning nothing

---
+++ csharp/class_practice que/without parameter and returing nothing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace without 
+{
+    class Program
+    {
+        static void display()
+            //method without parameters and returning nothing
+            //wap to create a method display without parameters
+        {
+            Console.WriteLine("hello from display method");
+        }
+        static void Main(string[] args)
+        {
+            display();
+            Console.WriteLine("bye");
+            Console.ReadLine();
+        }
+        
+    }
+}
