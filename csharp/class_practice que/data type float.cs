669a506872a7b39e5e53f4981f311bcdfb4d3e54
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-15 23:28:18 +0530

    Add files via upload

---
+++ csharp/class_practice que/data type float.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_type_float
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float b1;
+            Console.WriteLine("enter float number ");
+            b1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("float entered = " + b1);
+
+            Console.ReadLine();
+        }
+    }
+}
