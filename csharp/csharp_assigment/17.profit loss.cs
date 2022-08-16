3f88cc8bd585362ca8dbcea011627bd50048b560
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-16 19:52:45 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/17.profit loss.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.Profit_loss
+{//17. Write a C# Sharp program to calculate profit and loss on a transaction.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cost, sell;
+            Console.WriteLine("Enter the cost price:");
+            cost = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the selling price:");
+            sell = Convert.ToInt32(Console.ReadLine());
+            if (cost > sell)
+            {
+                int profit=cost-sell;
+                Console.WriteLine("You can book your profit amount : " + profit);
+            }
+            else if (cost < sell)
+            {
+                int loss = sell - cost;
+                Console.WriteLine("You can book your loss amount : " + loss);
+            }
+            else
+            {
+                Console.WriteLine("no profit no loss");
+            }
+            Console.ReadLine();
+        }
+    }
+}
