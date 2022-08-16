6d0769ce739275a7650006f6ad68d0fab6381c2d
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-16 19:51:54 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/15.traingle valid or not.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.triangle_valid_or_not
+{//15. Write a C# Sharp program to check whether a triangle can be formed by the given value for the angles.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Enter 1 st angle measure:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2 nd angle measure:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3 rd angle measure:");
+            c = Convert.ToInt32(Console.ReadLine());
+            int sum = a + b + c;
+            if (sum == 180)
+            {
+                Console.WriteLine("The triangle is valid!");
+            }
+            else
+            {
+                Console.WriteLine("The triangle is not valid!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
