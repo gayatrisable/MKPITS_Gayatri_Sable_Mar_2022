117a3ff1e644e6be2da8a30ce5e0be5b55b8d496
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-16 19:31:38 +0530

    even or odd number

---
+++ csharp/csharp_assigment/2.even or odd number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.even_odd
+{
+    internal class Program
+    {//2. Write a C# Sharp program to check whether a given number is even or odd.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------check number the even or odd---------");
+            int a, res;
+            Console.WriteLine("Enter the number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            res = a % 2;
+            if (res==0)
+            {
+                Console.WriteLine(a + " This number is even!");
+            }
+            else
+            {
+                Console.WriteLine(a + " This number is odd!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
