1f4d7e0e8312c8ee5062dcb4318129412aa10b3f
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-16 19:37:34 +0530

    leap year

---
+++ csharp/csharp_assigment/4.leap year.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.leap_year_or_not
+{
+    internal class Program
+    {//4. Write a C# Sharp program to find whether a given year is a leap year or not.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------whether a given year is a leap year or not.------------");
+            int a;
+            Console.WriteLine("Enter the year:");
+            a = Convert.ToInt32(Console.ReadLine());
+            if( a % 4 == 0 )//or (a % 400==0 || a % 4 == 0 || a % 100 == 0)
+            {
+                Console.WriteLine(a + " leap year!");
+            }
+            else
+            {
+                Console.WriteLine(a + " not leap  year!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
