382fbc560d62f352d800af2c9bb3da0c4083c344
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-16 19:52:19 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/16.vowel constant or not.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.vowel_or_consonant
+{//16. Write a C# Sharp program to check whether an alphabet is a vowel or consonant.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;
+            Console.WriteLine("Enter the Character: ");
+            ch = Convert.ToChar(Console.ReadLine());
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' )
+            {
+                Console.WriteLine("The alphabet is a Vowel!");
+            }
+            else
+            {
+                Console.WriteLine("The alphabet is a consonant!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
