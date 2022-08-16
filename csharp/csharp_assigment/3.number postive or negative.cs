913a1745d087ad7814541c9c7aed7256f927677a
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-16 19:33:29 +0530

    number positive or negative

---
+++ csharp/csharp_assigment/3.number postive or negative.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.number_postive_and_negative
+{
+    internal class Program
+    {//3. Write a C# Sharp program to check whether a given number is positive or negative.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------- check whether a given number is positive or negative.-------------");
+            int a;
+            Console.WriteLine("Enter the number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            if(a>=0)
+            {
+                Console.WriteLine(a + " is postive");
+            }
+            else
+            {
+                Console.WriteLine(a + " is Negative");
+            }
+            Console.ReadLine();
+        }
+    }
+}
