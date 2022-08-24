c7ba17c7d9065ec04bc96fe251e72c6158d116d3
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-24 12:02:41 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/number increased by 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number_increased_by_1
+{//12. Write a program in C# Sharp to make such a pattern like right angle triangle with number increased by 1.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j,k=1;
+            
+            for (i = 1; i <= 10; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0} ",(k++));
+                Console.Write("\n");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
