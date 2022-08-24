050a8d5615848294ef4cfaf71d4d065174fa33ac
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-24 12:06:15 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/table.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace table
+{//6. Write a program in C# Sharp to display the multiplication table of a given integer.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i;
+            Console.Write("Input the number (Table to be calculated : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=10;i++)
+            {
+                Console.WriteLine("{0} * {1} = {2}", n, i, (n * i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
