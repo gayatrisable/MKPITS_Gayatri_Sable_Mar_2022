c73ad289fe2e004f4f7aac1f612d88bfea76770e
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-24 12:03:50 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/right angle triangle using an asterisk.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace right_angle_triangle_using_an_asterisk
+{//9. Write a program in C# Sharp to display the pattern like right angle triangle using an asterisk.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            // Console.Write("Input number of rows : ");
+            //rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= 4; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
