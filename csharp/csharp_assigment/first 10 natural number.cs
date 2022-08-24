78fb6bc267c983ac0a1e1ca2b14e7f6939f0746e
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-24 12:01:34 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/first 10 natural number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_10_natural_number
+{//1. Write a program in C# Sharp to display the first 10 natural numbers.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=10;i++)
+            {
+                Console.WriteLine(i);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
