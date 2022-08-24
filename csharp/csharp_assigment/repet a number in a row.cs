035582fb7d2ecbd1ebe75b5f730a582406a51fb7
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-08-24 12:03:02 +0530

    Add files via upload

---
+++ csharp/csharp_assigment/repet a number in a row.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repeat_a_number_in_a_row
+{//11. Write a program in C# Sharp to make such a pattern like right angle triangle with a number which will repeat a number in a row.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+           
+            for (i = 1; i <= 8; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write(i);
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
