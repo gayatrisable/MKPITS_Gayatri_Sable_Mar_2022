86720bee23c2394ef0170ab2df5971af14dbd582
gayatrisable <gayatrisable19@gmail.com>
2022-09-01 23:36:07 +0530

    class console array roll no and name

---
+++ csharp/Windows form application/student roll no and name/student roll no and name/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Collections;
+
+namespace student_roll_no_and_name
+{
+    class student
+    {
+        public int rno;
+        public String name;
+        public student(int rno, String name)
+        {
+            this.rno = rno;
+            this.name = name;
+        }
+    }
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student(12, "amit");
+            student s2 = new student(14, "raj");
+            student s3 = new student(16, "raman");
+            ArrayList al = new ArrayList();
+            al.Add(s1);
+            al.Add(s2);
+            al.Add(s3);
+            //displaying the values of student
+            foreach (student s in al)
+            {
+                Console.WriteLine("rno " + s.rno);
+                Console.WriteLine("name  " + s.name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
