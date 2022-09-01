02d0916d696f33dfe25e06a194500b9d4687c6d0
gayatrisable <gayatrisable19@gmail.com>
2022-09-01 20:13:53 +0530

    array list 1 std info

---
+++ csharp/Windows form application/aaray list 1 std info/aaray list 1 std info/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace aaray_list_1_std_info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList ar = new ArrayList();
+            ar.Add("amit");
+            ar.Add("raj");
+            ar.Add("dilip");
+            Console.WriteLine("Student Name : ");
+            //displaying values of arraylist
+            foreach (Object obj in ar)
+            {
+                Console.WriteLine( obj);
+            }
+            Console.ReadLine();
+        
+        }
+    }
+}
