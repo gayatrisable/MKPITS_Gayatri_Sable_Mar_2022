f3a67ee41d59bbc06c1044f35b27f44ddbef0035
gayatrisable <gayatrisable19@gmail.com>
2022-09-03 21:00:41 +0530

    check smallest number assigment 1

---
+++ csharp/Windows form application/1 smallest number/1 smallest number/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_smallest_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            if (n1 < n2)
+            {
+                label3.Text = "Smaller Number is :" + n1;
+            }
+            else
+            {
+                label3.Text = "Smaller Number is : " + n2;
+            }
+        }
+    }
+}
