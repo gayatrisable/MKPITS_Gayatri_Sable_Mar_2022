4d1cc6f87695f657506dd5b69d824c83dc12a881
gayatrisable <gayatrisable19@gmail.com>
2022-09-14 19:57:59 +0530

    1_ass

---
+++ csharp/Windows form application/1.ass class result/1.ass class result/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1.ass_class_result
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Result res = new Result();
+            
+            int total = res.calculatetotal(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            float per = res.calculatepercentage(Convert.ToInt32(total));
+            string grade = res.calculategrade(Convert.ToSingle(per));
+            label4.Text = "total = "+total.ToString();
+            label4.Visible = true;
+            label5.Text = "Persentage = "+per.ToString();
+            label6.Text = "Grade = "+grade;
+        }
+    }
+}
---
+++ csharp/Windows form application/1.ass class result/1.ass class result/Result.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.ass_class_result
+{
+     class Result
+    {
+        
+        public int calculatetotal(int phy,int chem,int math)
+        {
+        
+        int total = chem + phy + math;
+            return  total;
+        }
+        public float calculatepercentage(int total)
+        {
+            float per = (total / 300.0f) * 100;
+            return  per;
+        }
+        
+        public string calculategrade(float per)
+        {
+           
+            if (per >= 75)
+            {
+                return " Distinction ";
+            }
+            else if (per >= 65)
+            {
+                return " first class ";
+            }
+            else if (per >= 45)
+            {
+                return " Second class ";
+            }
+            else
+            {
+                return " fail ";
+            }
+        }
+
+    }
+}
+    
+
