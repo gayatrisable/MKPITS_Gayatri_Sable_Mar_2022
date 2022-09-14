925c1ceca62de4c08ccdac13055ef56c23b3e6fd
gayatrisable <gayatrisable19@gmail.com>
2022-09-14 20:31:14 +0530

    windows_form_calculate

---
+++ csharp/Windows form application/calculate1/calculate1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using calculate;
+
+namespace calculate1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        calclate cal = new calclate();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int res = cal.addition(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            label3.Text = "Addition : " + res;
+            int res1 = cal.substraction(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            label4.Text = "Substraction : " + res1;
+        }
+
+      
+    }
+}
