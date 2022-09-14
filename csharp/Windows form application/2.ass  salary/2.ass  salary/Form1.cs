8bc1e7a3d09f88abc06470cbd0fd744d3947843b
gayatrisable <gayatrisable19@gmail.com>
2022-09-14 20:05:57 +0530

    2_ass_salary

---
+++ csharp/Windows form application/2.ass  salary/2.ass  salary/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2.ass__salary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            employee emp = new employee();
+            float bonous = emp.calculatebonus(Convert.ToInt32(textBox1.Text));
+            float totalsal = emp.calulatetotalsalary(Convert.ToSingle(bonous), Convert.ToInt32(textBox3.Text));
+            emp.empno = Convert.ToInt32(textBox1.Text);
+            emp.empname = textBox2.Text;
+            label4.Text ="Employee Number : " +emp.empno.ToString();
+            label5.Text = "Employee Name : "+emp.empname;
+            label6.Text = "Total Salary : " +totalsal.ToString();
+
+        }
+    }
+}
---
+++ csharp/Windows form application/2.ass  salary/2.ass  salary/employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.ass__salary
+{
+     class employee
+    {
+        public int empno;
+        public string empname;
+        
+
+        public float calculatebonus(int basicsal)
+        {
+            float bonus = basicsal * 0.45f;
+            return bonus;
+        }
+        public float calulatetotalsalary(float bonus,int basicsa)
+        {
+            float totalsal = bonus + basicsa;
+            return totalsal;
+        }
+    }
+}
