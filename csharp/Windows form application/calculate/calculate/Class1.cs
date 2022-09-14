49c565b4be6e7b4c72e8ec39a9eb00568d292ecc
gayatrisable <gayatrisable19@gmail.com>
2022-09-14 20:18:42 +0530

    dll_file_calculate

---
+++ csharp/Windows form application/calculate/calculate/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculate
+{
+    public class calclate
+    {
+        public int addition(int n1, int n2)
+        {
+            int result;
+            result = n1 + n2;
+            return result;
+        }
+        public int substraction(int n1, int n2)
+        {
+            int result;
+            result = n1 - n2;
+            return result;
+
+        }
+    }
+}
