d77786097fb75e1b6e3e55aec446210f12ac28b9
gayatrisable <gayatrisable19@gmail.com>
2022-09-14 20:02:16 +0530

    1_windows_classes

---
+++ csharp/Windows form application/1.windows classes/1.windows classes/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1.windows_classes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            student s = new student();
+            s.rno = Convert.ToInt32(textBox1.Text);
+            s.name = textBox2.Text;
+            s.email = textBox3.Text;
+            s.mobileno = Convert.ToInt64(textBox4.Text);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Roll Number : " + s.rno + "\n");
+            sb.Append("Student Name : " + s.name + "\n");
+            sb.Append("email :" + s.email + "\n");
+            sb.Append("Mobile no : " + s.mobileno + "\n");
+            label5.Text = sb.ToString();
+
+        }
+    }
+}
---
+++ csharp/Windows form application/1.windows classes/1.windows classes/student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.windows_classes
+{
+    class student
+        {
+            public int rno;
+            public string name;
+            public string email;
+            public long mobileno;
+
+        }
+
+    }
+
