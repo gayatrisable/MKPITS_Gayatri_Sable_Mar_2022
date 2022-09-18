fbcfc169892c0fdf3e1e9d3587b9737be87e8c47
gayatrisable <gayatrisable19@gmail.com>
2022-09-18 23:24:22 +0530

    UI_control_tab_control

---
+++ csharp/Windows form application/Tab Control/Tab Control/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Tab_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Registraction = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Account = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.Registraction.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.Account.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Registraction
+            // 
+            this.Registraction.Controls.Add(this.tabPage1);
+            this.Registraction.Controls.Add(this.Account);
+            this.Registraction.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registraction.Location = new System.Drawing.Point(113, 57);
+            this.Registraction.Name = "Registraction";
+            this.Registraction.SelectedIndex = 0;
+            this.Registraction.Size = new System.Drawing.Size(535, 335);
+            this.Registraction.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(527, 302);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Register";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Account
+            // 
+            this.Account.Controls.Add(this.label2);
+            this.Account.Location = new System.Drawing.Point(4, 29);
+            this.Account.Name = "Account";
+            this.Account.Padding = new System.Windows.Forms.Padding(3);
+            this.Account.Size = new System.Drawing.Size(527, 302);
+            this.Account.TabIndex = 1;
+            this.Account.Text = "Account";
+            this.Account.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "reg";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(188, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Account";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Registraction);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Registraction.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.Account.ResumeLayout(false);
+            this.Account.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Registraction;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage Account;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
