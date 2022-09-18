1881e46bba739423973e4006861798b209a6433d
gayatrisable <gayatrisable19@gmail.com>
2022-09-18 23:12:51 +0530

    UI_control

---
+++ csharp/Windows form application/checked list box/checked list box/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checked_list_box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+            {
+                sb.Append(checkedListBox1.CheckedItems[i] + "\n");
+            }
+            label1.Text = " Courses \n"+sb.ToString();
+        }
+    }
+}
