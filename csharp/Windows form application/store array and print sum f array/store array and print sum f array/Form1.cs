99614bfb66a78aa6a63c1e3e971aca9265fb7d30
gayatrisable <gayatrisable19@gmail.com>
2022-09-18 22:55:03 +0530

    2D_array_store_array

---
+++ csharp/Windows form application/store array and print sum f array/store array and print sum f array/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace store_array_and_print_sum_f_array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //creating an array of  5 elements
+        int[] num = new int[5];
+        int i = 0;
+        
+        int sum = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i < 5)
+            {
+                num[i] = Convert.ToInt32(textBox1.Text);
+                sum = sum + num[i];
+                i++;
+                textBox1.Clear();
+                textBox1.Focus();
+                if (i == 5)
+                {
+                    label2.Text = "sum of 5 no." + sum;
+                }
+            }
+        }
+    }
+}
