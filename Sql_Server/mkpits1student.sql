5be8dbe982df54a2ab7f190533de892d29b63b8a
gayatrisable <102215986+gayatrisable@users.noreply.github.com>
2022-10-03 14:29:36 +0530

    Add files via upload

---
+++ Sql_Server/mkpits1student.sql
@@ -0,0 +1,13 @@
+create table student3(rno int,name varchar(20),address varchar(100),mobileno bigint)
+insert into student3 values(11,'Amit','Flat no.346',8149947005)
+insert into student3 values(12,'Neha','Flat no.347',8149976989)
+insert into student3 values(13,'Nikita','Flat no.348',5678700598)
+insert into student3 values(14,'Gayatri','Flat no.349',849987005)
+insert into student3 values(15,'Ram','Flat no.350',8149947005)
+insert into student3 values(16,'Priyanka','Flat no.351',814994005)
+insert into student3 values(17,'om','Flat no.352',814547005)
+insert into student3 values(18,'Achal','Flat no.353',8677947005)
+insert into student3 values(19,'Shivani','Flat no.354',3455699405)
+insert into student3 values(20,'Maya','Flat no.355',455947005)
+select * from student3
+delete from student3 
